dd87f2444a016d756d31dcf907156eabf689d018
Tanay Mathur <tanay.mathur94@gmail.com>
2016-01-31 14:35:06 +0530

    Added registration

--- InstReg/urls.py
+++ InstReg/urls.py
@@ -13,9 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import include,url
 from django.contrib import admin
+from register.views import *
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', register),
+#     url(r'^logout/$', logout_page),
+#     url(r'^accounts/login/$', 'django.contrib.auth.views.login'), # If user is not login it will redirect to login page
+    url(r'^register/$', register),
+    url(r'^register/success/$', register_success),
+#     url(r'^home/$', home),
 ]
